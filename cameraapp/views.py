e9deb035f0d1583bd3bc35190241755983dc7f07
moritz-petzka-external_jhdev <moritz.petzka.external@jungheinrich.de>
2025-04-26 14:28:42 +0200

    refactored all functions

--- cameraapp/views.py
+++ cameraapp/views.py
@@ -26,7 +26,7 @@ from django.contrib.auth import logout
 # project
 from .models import CameraSettings
 from .camera_core import (
-    init_camera, cap, camera_instance, apply_photo_settings,
+    init_camera, camera_instance, apply_photo_settings,
     apply_auto_settings, auto_adjust_from_frame, apply_cv_settings
 )
 from .recording_job import RecordingJob
@@ -409,8 +409,8 @@ def update_camera_settings(request):
             settings_obj.save()
 
             print("[UPDATE_CAMERA_SETTINGS] Neue Einstellungen gespeichert. Kamera wird neu initialisiert.")
-            if cap and cap.isOpened():
-                cap.release()
+            if camera_instance and camera_instance.isOpened():
+                camera_instance.release()
             init_camera()
         else:
             print("[UPDATE_CAMERA_SETTINGS] Kein CameraSettings-Objekt vorhanden.")
