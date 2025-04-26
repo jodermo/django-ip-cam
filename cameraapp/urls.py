ba253f230558f2649aca180ee67eb8891ce2846e
moritz-petzka-external_jhdev <moritz.petzka.external@jungheinrich.de>
2025-04-26 09:11:54 +0200

    added record function to live stream

--- cameraapp/urls.py
+++ cameraapp/urls.py
@@ -5,11 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.stream_page, name="stream_page"),
     path("video_feed/", views.video_feed, name="video_feed"),
+    path('start_recording/', views.start_recording, name='start_recording'),
+    path('stop_recording/', views.stop_recording, name='stop_recording'),
     path("record_video/", views.record_video, name="record_video"),
     path("gallery/", views.photo_gallery, name="photo_gallery"),
     path("settings/", views.settings_view, name="settings_view"),
     path("accounts/logout/", views.logout_view, name="logout"),
     path("reboot/", views.reboot_pi, name="reboot_pi"),
-    path("start_recording/", views.start_recording, name="start_recording"),
-    path("stop_recording/", views.stop_recording, name="stop_recording"),
 ]
