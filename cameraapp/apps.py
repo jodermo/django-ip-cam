b54a08d34e6eeff5c8430a227dc4a79f5af63444
moritz-petzka-external_jhdev <moritz.petzka.external@jungheinrich.de>
2025-04-27 23:08:53 +0200

    new structure working 34

--- cameraapp/apps.py
+++ cameraapp/apps.py
@@ -1,5 +1,3 @@
-# cameraapp/apps.py
-
 from django.apps import AppConfig
 import os
 import threading
@@ -11,16 +9,16 @@ class CameraAppConfig(AppConfig):
     def ready(self):
         run_timelapse = os.environ.get("RUN_TIMELAPSE", "1") == "1"
         print(f"[CAMERA_APP] App ready. RUN_TIMELAPSE = {run_timelapse}")
-        threading.Thread(target=start_photo_scheduler, daemon=True).start()
-        if run_timelapse:
-            try:
-                from .scheduler import start_photo_scheduler
-                from .camera_utils import start_camera_watchdog
 
+        try:
+            from .scheduler import start_photo_scheduler
+            from .camera_utils import start_camera_watchdog
+
+            if run_timelapse:
                 print("[CAMERA_APP] Starting timelapse scheduler thread...")
-                # threading.Thread(target=start_photo_scheduler, daemon=True).start()
+                threading.Thread(target=start_photo_scheduler, daemon=True).start()
 
-                print("[CAMERA_APP] Starting camera watchdog...")
-                start_camera_watchdog()
-            except Exception as e:
-                print(f"[CAMERA_APP] Failed to start scheduler/watchdog: {e}")
+            print("[CAMERA_APP] Starting camera watchdog...")
+            start_camera_watchdog()
+        except Exception as e:
+            print(f"[CAMERA_APP] Failed to start scheduler/watchdog: {e}")
