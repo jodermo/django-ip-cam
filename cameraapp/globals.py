8d5940120bb9f33a148020e50bf78c8c0a0641c5
moritz-petzka-external_jhdev <moritz.petzka.external@jungheinrich.de>
2025-04-27 21:22:08 +0200

    new structure working 2

--- cameraapp/globals.py
+++ cameraapp/globals.py
@@ -1,17 +1,23 @@
 # cameraapp/globals.py
 
-
 import threading
 
-camera_lock = threading.Lock()
-latest_frame = None
-latest_frame_lock = threading.Lock()
-livestream_resume_lock = threading.Lock()
-livestream_lock = threading.Lock()
-livestream_job = None
-taking_foto = False
-recording_job = None
-active_stream_viewers = 0
-last_disconnect_time = None
-recording_timeout = 30
-camera = None
+
+class AppGlobals:
+    def __init__(self):
+        self.camera_lock = threading.Lock()
+        self.latest_frame = None
+        self.latest_frame_lock = threading.Lock()
+        self.livestream_resume_lock = threading.Lock()
+        self.livestream_lock = threading.Lock()
+        self.livestream_job = None
+        self.taking_foto = False
+        self.recording_job = None
+        self.active_stream_viewers = 0
+        self.last_disconnect_time = None
+        self.recording_timeout = 30
+        self.camera = None
+
+
+# Singleton Instanz für globale App-Zustände
+app_globals = AppGlobals()
